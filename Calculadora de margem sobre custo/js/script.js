f1dc450199a78849a49a6a317944bf98048c348a
Alnfonseca <afonsecadesantana@gmail.com>
2024-02-23 19:04:31 -0300

    arrumar botão

--- Calculadora de margem sobre custo/js/script.js
+++ Calculadora de margem sobre custo/js/script.js
@@ -1,3 +1,4 @@
+
 function custodoproduto() {
  //custo do produto 
     var valorCusto = document.querySelector("#Custo").value;
@@ -7,9 +8,27 @@ function custodoproduto() {
   
   function porcentagem(){
     var mark = document.querySelector("#Porcentagem").value;
-    var valorporcem = mark * 1/100
+    var valorporcem = mark * 1/100 + "%";
 
     document.querySelector('#mark').textContent = valorporcem;
   }
 
-console.log("Ola mundo");
+  function custocomarkup(){
+    var customark = porcentagem + 1 * custodoproduto;
+   
+    document.querySelector("#custoComMarkUp").textContent = customark;
+  }
+
+
+
+
+  /*function mostrar(event){
+    event.preventDefault();
+    var custo = Number(document.querySelector("#Custo").value);
+    document.querySelector("#Valor").textContent = custo.toFixed(2);
+  
+    var mark = document.querySelector("#Porcentagem").value;
+    var porcentagem = mark/100;
+  
+    document.querySelector("#mark").textContent = porcentagem + '%';
+  }*/

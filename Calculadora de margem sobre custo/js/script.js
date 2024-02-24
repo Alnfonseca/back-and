d1adba97aa0bee7ac3f513d9df58172a0c83a48e
Alnfonseca <afonsecadesantana@gmail.com>
2024-02-24 17:34:13 -0300

    finalizado

--- Calculadora de margem sobre custo/js/script.js
+++ Calculadora de margem sobre custo/js/script.js
@@ -1,34 +1,54 @@
+function calcular(event){
+  event.preventDefault();
 
-function custodoproduto() {
- //custo do produto 
-    var valorCusto = document.querySelector("#Custo").value;
-  
-    document.querySelector("#Valor").textContent = valorCusto;
-  }
-  
-  function porcentagem(){
-    var mark = document.querySelector("#Porcentagem").value;
-    var valorporcem = mark * 1/100 + "%";
+  var custo = Number(document.querySelector("#Custo").value);
+  document.querySelector("#valor").textContent = custo.toFixed(2);
 
-    document.querySelector('#mark').textContent = valorporcem;
-  }
+  var markUp = document.querySelector("#markup").value;
+  var porcentagem = markUp/100 
 
-  function custocomarkup(){
-    var customark = porcentagem + 1 * custodoproduto;
-   
-    document.querySelector("#custoComMarkUp").textContent = customark;
-  }
+  document.querySelector("#mark").textContent = (porcentagem*100).toFixed(2) + '%';
 
+}
 
+function custocommark(){
+  var custo = document.querySelector("#Custo").value;
+  var markUp = document.querySelector("#markup").value;
+  var porcentagem = markUp/100
 
+  var custoMark = custo*(1+porcentagem) 
+  document.querySelector("#custo-mark").textContent = custoMark.toFixed(2)
+}
 
-  /*function mostrar(event){
-    event.preventDefault();
-    var custo = Number(document.querySelector("#Custo").value);
-    document.querySelector("#Valor").textContent = custo.toFixed(2);
-  
-    var mark = document.querySelector("#Porcentagem").value;
-    var porcentagem = mark/100;
-  
-    document.querySelector("#mark").textContent = porcentagem + '%';
-  }*/
+function venda(){
+  var valor = document.querySelector("#custo-mark").textContent;
+  var preco = valor/(1-(18/100)-(9.25/100));
+  document.querySelector("#preco").textContent =preco.toFixed(2);
+}
+
+function icms(){
+  var venda = document.querySelector("#preco").textContent *(18/100)
+  document.querySelector("#icms").textContent = venda.toFixed(2)
+}
+
+function piscof(){
+  var venda = document.querySelector("#preco").textContent *(9.25/100);
+
+  document.querySelector("#pis").textContent = venda.toFixed(2);
+}
+
+function ipi(){
+  var venda = document.querySelector("#preco").textContent *(1 + (15/100))
+  document.querySelector("#ipi").textContent = venda.toFixed(2)
+}
+
+function lucro(){
+  var valor = document.querySelector("#custo-mark").textContent - document.querySelector("#valor").textContent;
+
+  document.querySelector("#margem").textContent = valor.toFixed(2)
+}
+
+function margemlucro(){
+  var lucro = document.querySelector("#margem").textContent/document.querySelector("#preco").textContent
+
+  document.querySelector("#margem-porcentagem").textContent = (lucro*100).toFixed(2) + '%'}
